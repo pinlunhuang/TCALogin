576e7506dc485c48eded8b3c5dfdc4452c74bbde
pinlun <lunwaac@gmail.com>
2025-03-15 13:55:22 +0800

    Initial commit

---
+++ TCALogin/TCALoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCALoginApp.swift
+//  TCALogin
+//
+//  Created by Pinlun Huang on 2025/3/15.
+//
+
+import SwiftUI
+
+@main
+struct TCALoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
